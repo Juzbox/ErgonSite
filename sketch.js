a986396c0e467cc20a53f6f65de6b4feb1b719f7
Juzbox <hugopilate@gmail.com>
2020-11-06 20:38:25 +0100

    calculator update

--- sketch.js
+++ sketch.js
@@ -9,11 +9,12 @@ let valWa;
 let valP;
 let valWe;
 let valH;
-
+let valMo;
 
 let totalCost;
 let totalEffort;
 let plantLife;
+let plantLife2;
 let workerMood;
 let kalitat;
 
@@ -38,6 +39,8 @@ let imgP1, imgP2, imgP3;
 let imgL1, imgL2, imgL3;
 let imgH1, imgH2, imgH3;
 
+let imgB;
+
 
 
 let mapL, mapH, mapW, mapP;
@@ -48,24 +51,29 @@ let sliderH1, sliderH2, sliderH3, sliderH4, sliderH5;
 
 
 function setup() {
-    
-    
-   var canvas = createCanvas(800, 600);
+
+
+    var canvas = createCanvas(800, 600);
     canvas.parent('calc');
 
+
+
     leftMargin = 100;
-    scoreMargin = 580;
+    scoreMargin = 540;
     sliderX = 1070;
     sliderYgap = 30;
-    sliderSize = 200;
+    sliderSize = 100;
     textSize(15);
     tempH = innerHeight - 300;
     imgH = 300;
     sliderY = -65;
-captionAdjust=585;
-    
+    captionAdjust = 545;
+
     UIgap = 350;
-    UIstartY = innerHeight*4+340;
+    UIstartY = innerHeight * 4 + 340;
+
+
+
 
     sliderH1 = UIgap;
     sliderH2 = UIgap + sliderYgap;
@@ -73,7 +81,7 @@ captionAdjust=585;
     sliderH4 = UIgap + sliderYgap * 3;
     sliderH5 = UIgap + sliderYgap * 4;
 
-    
+
 
     imgBASE = loadImage('assets/exports1/baseImg.png');
 
@@ -92,32 +100,20 @@ captionAdjust=585;
     imgH1 = loadImage('assets/exports1/H1.png');
     imgH2 = loadImage('assets/exports1/H2.png');
     imgH3 = loadImage('assets/exports1/H3.png');
+    
+        imgB = loadImage('assets/exports1/birds.png');
+
+    
+
+
+    sliderLeaves = new HScrollbar(captionAdjust-40, sliderH1, sliderSize, 16, 2);
+    sliderWeeds = new HScrollbar(captionAdjust-40, sliderH2, sliderSize, 16, 2);
+    sliderPruning = new HScrollbar(captionAdjust-40, sliderH3, sliderSize, 16, 2);
+    sliderHedges = new HScrollbar(captionAdjust-40, sliderH4, sliderSize, 16, 2);
+    sliderWage = new HScrollbar(captionAdjust-40, sliderH5, sliderSize, 16, 2);
 
 
 
-
-
-
-    sliderLeaves = createSlider(1, 8, 1);
-    sliderLeaves.position(sliderX, UIstartY+sliderYgap * 2 + sliderY);
-    sliderLeaves.style('width', sliderSize);
-
-    sliderWeeds = createSlider(1, 8, 1);
-    sliderWeeds.position(sliderX, UIstartY+ sliderYgap * 3 + sliderY);
-    sliderWeeds.style('width', sliderSize);
-
-    sliderPruning = createSlider(1, 8, 1);
-    sliderPruning.position(sliderX, UIstartY+sliderYgap * 4 + sliderY);
-    sliderPruning.style('width', sliderSize);
-
-    sliderHedges = createSlider(1, 8, 1);
-    sliderHedges.position(sliderX, UIstartY+ sliderYgap * 5 + sliderY);
-    sliderHedges.style('width', sliderSize);
-
-
-    sliderWage = createSlider(10, 20, 10);
-    sliderWage.position(sliderX, UIstartY+ sliderYgap * 6 + sliderY);
-    sliderWage.style('width', sliderSize);
 
 
 }
@@ -129,84 +125,99 @@ function draw() {
 
     background(255, 255, 255);
 
-    valL = sliderLeaves.value();
-    valWe = sliderWeeds.value();
-    valP = sliderPruning.value();
-    valH = sliderHedges.value();
-    valWa = sliderWage.value();
+
+
+
+    sliderLeaves.update();
+    sliderLeaves.display();
+      sliderWeeds.update();
+    sliderWeeds.display();
+      sliderHedges.update();
+    sliderHedges.display();
+      sliderWage.update();
+    sliderWage.display();
+  sliderPruning.update();
+    sliderPruning.display();
+
+    valL = int(map(sliderLeaves.getPos().toFixed(2), 560, 715, -1, 9));
+    valWe = int(map(sliderWeeds.getPos().toFixed(2), 560, 715, -1, 9));
+    valP = int(map(sliderPruning.getPos().toFixed(2), 560, 715, -1, 9));
+    valH = int(map(sliderHedges.getPos().toFixed(2), 560, 715, -1, 9));
+    valWa = int(map(sliderWage.getPos().toFixed(2), 560, 715, 6, 22));
 
     totalEffort = valL + valH + valWe + valP;
-    totalCost = (totalEffort) * valWa;
+    totalCost = truncateDecimals(valMo,2);
 
     image(imgBASE, leftMargin, 0, 600, imgH);
 
 
 
-    mapH = map(valH, 1, 8, 3, 1);
-    mapL = map(valL, 1, 8, 3, 1);
-    mapP = map(valP, 1, 8, 3, 1);
-    mapW = map(valWe, 1, 8, 3, 1);
+    mapH = valH;
+    mapL = valL;
+    mapP = valP;
+    mapW = valWe;
 
 
-       if (mapP === 1) {
-        image(imgP1, leftMargin, 0, 600, imgH);
+    if (mapP < 4) {
+        image(imgP3, leftMargin, 0, 600, imgH);
     }
 
-    if ((mapP > 1) && (mapP < 3)) {
+    if ((mapP >= 4) && (mapP < 7)) {
         image(imgP2, leftMargin, 0, 600, imgH);
     }
 
-    if (mapP === 3) {
-        image(imgP3, leftMargin, 0, 600, imgH);
-    }
-    
-
-    
-
-    if (mapW === 1) {
-        image(imgW1, leftMargin, 0, 600, imgH);
+    if (mapP >=7) {
+        image(imgP1, leftMargin, 0, 600, imgH);
     }
 
-    if ((mapW > 1) && (mapW < 3)) {
-        image(imgW2, leftMargin, 0, 600, imgH);
-    }
 
-    if (mapW === 3) {
+
+
+    if (mapW < 4) {
         image(imgW3, leftMargin, 0, 600, imgH);
     }
 
-    
-    
-    
-    if (mapH === 1) {
-        image(imgH1, leftMargin, 0, 600, imgH);
+    if ((mapW >= 4) && (mapW < 7)) {
+        image(imgW2, leftMargin, 0, 600, imgH);
     }
 
-    if ((mapH > 1) && (mapH < 3)) {
-        image(imgH2, leftMargin, 0, 600, imgH);
+    if (mapW >=7) {
+        image(imgW1, leftMargin, 0, 600, imgH);
     }
 
-    if (mapH === 3) {
+    let tempWage=valWa*totalEffort;
+valMo = map(tempWage, 20, 740, 0.20, 3);
+
+
+    if (mapH < 4) {
         image(imgH3, leftMargin, 0, 600, imgH);
     }
 
-
-    
-
-    if (mapL === 1) {
-        image(imgL1, leftMargin, 0, 600, imgH);
+    if ((mapH >= 4) && (mapH < 7)) {
+        image(imgH2, leftMargin, 0, 600, imgH);
     }
 
-    if ((mapL > 1) && (mapL < 3)) {
-        image(imgL2, leftMargin, 0, 600, imgH);
+    if (mapH >=7) {
+        image(imgH1, leftMargin, 0, 600, imgH);
     }
 
-    if (mapL === 3) {
+
+
+
+    if (mapL < 4) {
         image(imgL3, leftMargin, 0, 600, imgH);
     }
 
+    if ((mapL >= 4) && (mapL < 7)) {
+        image(imgL2, leftMargin, 0, 600, imgH);
+    }
 
- 
+    if (mapL >=7) {
+        image(imgL1, leftMargin, 0, 600, imgH);
+    }
+
+
+
 
 
 
@@ -217,20 +228,25 @@ function draw() {
 
 
     //PLANTLIFE
-    if ((valWe > 3) && (valWe < 6) && (valP > 3)) {
+    if ((valWe >3) && (valWe <7) && (valP < 6)) {
         plantLife = "🌿🌿🌿";
+        plantLife2 = "thriving";
+          image(imgB, leftMargin, 0, 600, imgH);
     } else {
-        if ((valWe > 6)) {
+        if ((valWe < 4)) {
             plantLife = "🌿🌿";
+            plantLife2 = "healthy";
         }
-        if ((valWe < 3)) {
+        else  {
             plantLife = "🌿";
+            plantLife2 = "contained";
         }
     }
+    
 
 
     //WORKMOOD
-    if ((totalEffort > 20) && (totalCost < 240)) {
+    if ((totalEffort > 28) && (valWa < 15)) {
         workerMood = "😩";
     } else {
         workerMood = "😀";
@@ -243,60 +259,116 @@ function draw() {
 
     //KALITAT
 
-    if ((totalEffort > 25)) {
+    if ((totalEffort > 34)) {
+        kalitat = "A+";
+    }
+    if ((totalEffort > 30) && (totalEffort <= 34)) {
         kalitat = "A";
     }
-    if ((totalEffort > 20) && (totalEffort <= 25)) {
+    if ((totalEffort > 26) && (totalEffort <= 30)) {
         kalitat = "B";
     }
-    if ((totalEffort > 16) && (totalEffort <= 20)) {
+    if ((totalEffort > 22) && (totalEffort <= 26)) {
         kalitat = "C";
     }
-    if ((totalEffort > 12) && (totalEffort <= 16)) {
+    if ((totalEffort <= 22)) {
         kalitat = "D";
     }
-    if ((totalEffort <= 12)) {
-        kalitat = "E";
-    }
 
-
+fill(255);
     textSize(32);
-    text(kalitat, scoreMargin, 70);
-    text(workerMood, scoreMargin, 120);
-    text(plantLife, scoreMargin, 170);
-    text(totalCost + "€", scoreMargin, 220);
+    text(kalitat, scoreMargin, 90);
+    text(workerMood, scoreMargin, 140);
+    text(plantLife, scoreMargin, 190);
+    text(totalCost + "€/mo.", scoreMargin, 240);
 
-
+fill(128,128,255);
 
     //slider labels
-    textSize(12);
-    text("How often should leaves be picked up? (times per month)", leftMargin, sliderH1);
-    text("How often should weeds be gathered?", leftMargin, sliderH2);
-    text("How often should low branches be pruned?", leftMargin, sliderH3);
-    text("How often should hedges be trimmed?", leftMargin, sliderH4);
-    text("How much should the gardeners be remunerated? (per hour)", leftMargin, sliderH5);
+    textSize(16);
+    text("Having Ergon gardeners pick leaves up " + valL + " times a month, weed " + valWe + " times a month, prune trees " + valP + " times a month, and trim hedges " + valH + " times a month for an hourly wage of " + valWa + "€/hr (which on average would add " + totalCost + "€ to your city taxes) would result in a "+ plantLife2+" plant life. The Kwaliteitscatalogus grade equivalent for this situation would be a "+ kalitat+".", leftMargin, sliderH1-10, 350, 2000);
 
-
+fill(128,128,255);
     //MINS
-    text("1", sliderX-captionAdjust - 25, sliderH1);
-    text("1", sliderX-captionAdjust - 25, sliderH2);
-    text("1", sliderX-captionAdjust - 25, sliderH3);
-    text("1", sliderX-captionAdjust - 25, sliderH4);
-    text("10eu", sliderX-captionAdjust - 35, sliderH5);
+    text("RAKING", sliderX - captionAdjust + 90, sliderH1+5);
+    text("WEEDING", sliderX - captionAdjust + 90, sliderH2+5);
+    text("PRUNING", sliderX - captionAdjust + 90, sliderH3+5);
+    text("TRIMMING", sliderX - captionAdjust + 90, sliderH4+5);
+    text("EARNING", sliderX - captionAdjust + 90, sliderH5+5);
 
 
 
-    //MAXS
-    text("8", sliderX-captionAdjust + sliderSize * 0.8, sliderH1);
-    text("8", sliderX-captionAdjust + sliderSize * 0.8, sliderH2);
-    text("8", sliderX-captionAdjust + sliderSize * 0.8, sliderH3);
-    text("8", sliderX-captionAdjust + sliderSize * 0.8, sliderH4);
-    text("20eu", sliderX-captionAdjust + sliderSize * 0.8, sliderH5);
 
-    //VALS
-    text(valL, sliderX-captionAdjust + sliderSize, sliderH1);
-    text(valWe, sliderX-captionAdjust + sliderSize, sliderH2);
-    text(valP, sliderX-captionAdjust + sliderSize, sliderH3);
-    text(valH, sliderX-captionAdjust + sliderSize, sliderH4);
-    text(valWa, sliderX-captionAdjust + sliderSize, sliderH5);
 }
+
+
+class HScrollbar {
+    constructor(xp, yp, sw, sh, l) {
+        this.swidth = sw;
+        this.sheight = sh;
+        let widthtoheight = sw - sh;
+        this.ratio = sw / widthtoheight;
+        this.xpos = xp;
+        this.ypos = yp - this.sheight / 2;
+        this.spos = this.xpos + this.swidth / 2 - this.sheight / 2;
+        this.newspos = this.spos;
+        this.sposMin = this.xpos;
+        this.sposMax = this.xpos + this.swidth - this.sheight;
+        this.loose = l;
+        this.over = false;
+        this.locked = false;
+    }
+
+    update() {
+        if (this.overEvent()) {
+            this.over = true;
+        } else {
+            this.over = false;
+        }
+        if (mouseIsPressed && this.over) {
+            this.locked = true;
+        }
+        if (!mouseIsPressed) {
+            this.locked = false;
+        }
+        if (this.locked) {
+            this.newspos = constrain(mouseX - this.sheight / 2, this.sposMin, this.sposMax);
+        }
+        if (abs(this.newspos - this.spos) > 1) {
+            this.spos = this.spos + (this.newspos - this.spos) / this.loose;
+        }
+    }
+
+    overEvent() {
+        if (mouseX > this.xpos && mouseX < this.xpos + this.swidth &&
+            mouseY > this.ypos && mouseY < this.ypos + this.sheight) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    display() {
+        noStroke();
+        fill(228,228,255);
+        rect(this.xpos, this.ypos, this.swidth, this.sheight);
+       
+          fill(128,128,255);
+        
+        rect(this.spos, this.ypos, this.sheight, this.sheight);
+    }
+
+    getPos() {
+        // Convert spos to be values between
+        // 0 and the total width of the scrollbar
+        return this.spos * this.ratio;
+    }
+}
+
+truncateDecimals = function (number, digits) {
+    var multiplier = Math.pow(10, digits),
+        adjustedNum = number * multiplier,
+        truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+
+    return truncatedNum / multiplier;
+};
